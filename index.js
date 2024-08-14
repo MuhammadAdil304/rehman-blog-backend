8e3cec378a8abad56993c67684197e96b1537516
MuhammadAdil304 <hafizmuhammadadil6@gmail.com>
2024-08-14 19:20:24 +0500

    work complete

--- index.js
+++ index.js
@@ -6,24 +6,17 @@ const userRoutes = require('./routes/user.route')
 const authRoutes = require('./routes/auth.route')
 const PostRoutes = require('./routes/post.route')
 const cookieParser = require('cookie-parser')
-const path = require('path')
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 dotenv.config()
 app.use(Cors({ origin: true, credentials: true }));
-const _dirname = path.resolve()
-
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', PostRoutes)
 
-app.use(express.static(path.join(_dirname, 'client/dist')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
-})
 
 
 mongoose
